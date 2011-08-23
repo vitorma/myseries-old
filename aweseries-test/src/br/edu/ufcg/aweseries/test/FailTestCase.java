68d64cb03e67e19422250a185e58b2c83f2dc533
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2011-08-23 05:23:12 -0300

    CI server test file. MUST be deleted ASAP

---
+++ aweseries-test/src/br/edu/ufcg/aweseries/test/FailTestCase.java
@@ -0,0 +1,16 @@
+package br.edu.ufcg.aweseries.test;
+
+import junit.framework.TestCase;
+
+//FIXME Remove this file ASAP, it is just a test
+
+public class FailTestCase extends TestCase {
+
+    public void testFail() {
+        fail();
+    }
+    
+    public void testPass() {
+        assertTrue(true);
+    }
+}
