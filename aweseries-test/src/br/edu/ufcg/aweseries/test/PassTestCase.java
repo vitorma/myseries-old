e5c7088cf3939c4b72280d545ff0c006c60c407d
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2011-08-23 05:40:50 -0300

    The test are now passing

--- aweseries-test/src/br/edu/ufcg/aweseries/test/PassTestCase.java
+++ aweseries-test/src/br/edu/ufcg/aweseries/test/PassTestCase.java
@@ -4,12 +4,8 @@ import junit.framework.TestCase;
 
 //FIXME Remove this file ASAP, it is just a test
 
-public class FailTestCase extends TestCase {
+public class PassTestCase extends TestCase {
 
-    public void testFail() {
-        fail();
-    }
-    
     public void testPass() {
         assertTrue(true);
     }
