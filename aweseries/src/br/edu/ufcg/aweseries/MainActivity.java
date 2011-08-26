838a6ea58e78658e533982429054f2a7e4698e23
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2011-08-26 15:38:13 -0300

    Creates stub for MySeries activity that shows the user's series.

--- aweseries/src/br/edu/ufcg/aweseries/MainActivity.java
+++ aweseries/src/br/edu/ufcg/aweseries/MainActivity.java
@@ -1,7 +1,11 @@
 package br.edu.ufcg.aweseries;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +13,14 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Button mySeries = (Button) findViewById(R.id.button1);
+        mySeries.setOnClickListener(new OnClickListener() {
+            
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MySeries.class));           
+            }
+        });
     }
 }
