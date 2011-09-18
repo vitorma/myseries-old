86d7d44bad362daa687b625bab214af144791328
Tiago A. Reul <reultiago@gmail.com>
2011-09-18 00:32:03 -0300

    "Adds aweseries-unittest project. This project should contain every portable (i.e. non plataform specific) test case for AweSeries. The classes SeriesDBTest, EnvironmentTest and UrlSupplierTest were ported to JUnit4 and added to this project. "

---
+++ aweseries-unittest/src/br/edu/ufcg/aweseries/test/unit/thetvdb/SeasonTest.java
@@ -0,0 +1,84 @@
+package br.edu.ufcg.aweseries.test.unit.thetvdb;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SeasonTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public final void testSeason() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public final void testAddEpisode() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public final void testGetEpisodes() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public final void testGetEpisodeAt() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public final void testGetNextEpisode() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public final void testGetNumber() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public final void testGetNumberOfEpisodes() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public final void testGetPoster() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public final void testIndexOf() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public final void testIsViewed() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public final void testMarkAllAsNotViewed() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public final void testMarkAllAsViewed() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public final void testMarkAsNotViewed() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public final void testMarkAsViewed() {
+        fail("Not yet implemented");
+    }
+
+}
