7b9102d0620641550fd6eceea9f1a69e9ca8547b
Cleber Goncalves de Sousa <cgs.ccc@gmail.com>
2011-09-20 00:05:00 -0300

    Adds class SeasonsParser.

--- aweseries/src/br/edu/ufcg/aweseries/thetvdb/Episode.java
+++ aweseries/src/br/edu/ufcg/aweseries/thetvdb/Episode.java
@@ -1,9 +1,19 @@
 package br.edu.ufcg.aweseries.thetvdb;
 
 public class Episode {
+	private String id;
     private boolean viewed;
+    private int seasonNumber;
 
-    public boolean isViewed() {
+    public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getId() {
+		return this.id;
+	}
+
+	public boolean isViewed() {
         return this.viewed;
     }
 
@@ -15,4 +25,19 @@ public class Episode {
         this.viewed = false;        
     }
 
+    public int getSeasonNumber() {
+    	return this.seasonNumber;
+    }
+
+    public void setSeasonNumber(int seasonNumber) {
+    	this.seasonNumber = seasonNumber;
+    }
+
+    public Episode copy() {
+    	Episode episode = new Episode();
+    	episode.id = this.id;
+    	episode.seasonNumber = this.seasonNumber;
+    	episode.viewed = this.viewed;
+		return episode;
+    }
 }
