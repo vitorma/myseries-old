21d0ce44cb33ef87487e2d26fac56a39bca04624
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2011-09-28 11:30:57 -0300

    TheTVDB now part of the App's Environment; Android stubs added to unittest, so mockito can mock them

--- aweseries-unittest/src/br/edu/ufcg/aweseries/test/unit/EnvironmentTest.java
+++ aweseries-unittest/src/br/edu/ufcg/aweseries/test/unit/EnvironmentTest.java
@@ -2,6 +2,7 @@ package br.edu.ufcg.aweseries.test.unit;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
 
 import org.junit.After;
 import org.junit.Before;
@@ -9,6 +10,7 @@ import org.junit.Test;
 
 import br.edu.ufcg.aweseries.Environment;
 import br.edu.ufcg.aweseries.SeriesProvider;
+import br.edu.ufcg.aweseries.thetvdb.TheTVDB;
 
 public class EnvironmentTest {
 
@@ -24,6 +26,7 @@ public class EnvironmentTest {
         this.environment = null;
     }
 
+    // Series Provider --------------------------------------------------------
     @Test
     public void testChangeSeriesProvider() {
         final SeriesProvider sp = SeriesProvider.newSeriesProvider();
@@ -48,8 +51,7 @@ public class EnvironmentTest {
     }
 
     @Test
-    public void
-            testSettingSeriesProviderToNullMustInstantiateANewSeriesProvider() {
+    public void testSettingSeriesProviderToNullMustInstantiateANewSeriesProvider() {
         final SeriesProvider oldSP = this.environment.getSeriesProvider();
         this.environment.setSeriesProvider(null);
 
@@ -57,5 +59,37 @@ public class EnvironmentTest {
                 notNullValue());
         assertThat(this.environment.getSeriesProvider(),
                 not(sameInstance(oldSP)));
+    }
+
+    // TheTVDB -----------------------------------------------------------------
+    @Test
+    public void setTheTVDB() {
+        final TheTVDB db = mock(TheTVDB.class);
+
+        this.environment.setTheTVDBTo(db);
+        assertThat(this.environment.theTVDB(), sameInstance(db));
+    }
+
+    @Test
+    public void firstTheTVDBNotNull() {
+        assertThat(this.environment.theTVDB(), notNullValue());
+    }
+
+    @Test
+    public void testReturnsSameTheTVDBEachCall() {
+        final TheTVDB db1 = this.environment.theTVDB();
+        final TheTVDB db2 = this.environment.theTVDB();
+
+        assertThat(db1, sameInstance(db2));
+    }
+
+    @Test
+    public void testSettingTheTVDBToNullMustInstantiateANewTheTVDB() {
+        final TheTVDB oldDB = this.environment.theTVDB();
+
+        this.environment.setTheTVDBTo(null);
+
+        assertThat(this.environment.theTVDB(), notNullValue());
+        assertThat(this.environment.theTVDB(), not(sameInstance(oldDB)));
     }
 }
--- aweseries/src/br/edu/ufcg/aweseries/Environment.java
+++ aweseries/src/br/edu/ufcg/aweseries/Environment.java
@@ -1,10 +1,17 @@
 package br.edu.ufcg.aweseries;
 
+import br.edu.ufcg.aweseries.thetvdb.TheTVDB;
+
 /**
  * Singleton repository. Stores all singleton objects. Allow dependency
  * injection.
  */
 public class Environment {
+
+    private SeriesProvider seriesProvider;
+    private TheTVDB theTVDB;
+
+    private final String apiKey = "6F2B5A871C96FB05";
 
     /**
      * If you know what you are doing, use this method to instantiate an
@@ -12,22 +19,13 @@ public class Environment {
      * @see Environment()
      * @see App.environment()
      */
-	public static Environment newEnvironment() {
-		return new Environment();
-	}
+    public static Environment newEnvironment() {
+        return new Environment();
+    }
     /**
      * @see newSeriesProvider()
      */
-	private Environment() {}
-
-    private SeriesProvider seriesProvider;
-
-    /**
-     * @return a default series provider for the production environment
-     */
-    private SeriesProvider defaultSeriesProvider() {
-        return SeriesProvider.newSeriesProvider();
-    }
+    private Environment() {}
 
     /**
      * @return the SeriesProvider for the app.
@@ -40,10 +38,32 @@ public class Environment {
     }
 
     /**
+     * @return a default series provider for the production environment
+     */
+    private SeriesProvider defaultSeriesProvider() {
+        return SeriesProvider.newSeriesProvider();
+    }
+
+    /**
      * Set the environment's series provider to newSeriesProvider.
      * If null, a new default series provider will be instantiated.
      */
     public void setSeriesProvider(SeriesProvider newSeriesProvider) {
         this.seriesProvider = newSeriesProvider;
+    }
+
+    public TheTVDB theTVDB() {
+        if (this.theTVDB == null) {
+            this.theTVDB = this.defaultTheTVDB();
+        }
+        return this.theTVDB;
+    }
+
+    private TheTVDB defaultTheTVDB() {
+        return new TheTVDB(this.apiKey);
+    }
+
+    public void setTheTVDBTo(TheTVDB newTheTVDB) {
+        this.theTVDB = newTheTVDB;
     }
 }
