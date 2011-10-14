466ffa0a5ecc624c747b9afcda0d42862636da28
Tiago A. Reul <reultiago@gmail.com>
2011-10-14 00:21:53 -0300

    Adds an options menu with the option 'Add Series' to MySeries. When clicked, the icon launches a view which allows one to search series by name.

--- aweseries/src/br/edu/ufcg/aweseries/gui/MySeries.java
+++ aweseries/src/br/edu/ufcg/aweseries/gui/MySeries.java
@@ -6,7 +6,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -32,8 +36,7 @@ public class MySeries extends Activity {
         setContentView(R.layout.listing);
 
         // set view title
-        TextView listingTitle
-            = ((TextView) findViewById(R.id.listingTitleTextView));
+        TextView listingTitle = ((TextView) findViewById(R.id.listingTitleTextView));
         listingTitle.setText("My Series");
 
         // rounded with try-catch block to debbug purposes
@@ -46,39 +49,38 @@ public class MySeries extends Activity {
     }
 
     class SeriesItemViewAdapter extends ArrayAdapter<Series> {
-		private final SeriesProvider seriesProvider = App.environment().seriesProvider();
-		
-		public SeriesItemViewAdapter(Context context, int seriesItemResourceId,
-				Series[] objects) {
-			super(context, seriesItemResourceId, objects);
-		}
-		
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			View itemView = convertView;
+        private final SeriesProvider seriesProvider = App.environment().seriesProvider();
 
-			// if no view was passed, create one for the item
-			if (itemView == null) {
-				LayoutInflater vi = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        public SeriesItemViewAdapter(Context context, int seriesItemResourceId, Series[] objects) {
+            super(context, seriesItemResourceId, objects);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View itemView = convertView;
+
+            // if no view was passed, create one for the item
+            if (itemView == null) {
+                LayoutInflater vi = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 itemView = vi.inflate(R.layout.list_item, null);
-			}
-			
-			// get views for the series fields
-			ImageView image = (ImageView) itemView.findViewById(R.id.itemSeriesImage);
-			TextView name = (TextView) itemView.findViewById(R.id.itemSeriesName);
-                        TextView status = (TextView) itemView.findViewById(R.id.itemSeriesStatus);
-			
-			// load series data
+            }
+
+            // get views for the series fields
+            ImageView image = (ImageView) itemView.findViewById(R.id.itemSeriesImage);
+            TextView name = (TextView) itemView.findViewById(R.id.itemSeriesName);
+            TextView status = (TextView) itemView.findViewById(R.id.itemSeriesStatus);
+
+            // load series data
             Series item = this.getItem(position);
 
             name.setText(item.getName());
             status.setText(item.getStatus());
 
-			Bitmap poster = this.seriesProvider.getPosterOf(item);
-			image.setImageBitmap(poster);
+            Bitmap poster = this.seriesProvider.getPosterOf(item);
+            image.setImageBitmap(poster);
 
-			return itemView;
-		}
+            return itemView;
+        }
     }
 
     /**
@@ -87,8 +89,8 @@ public class MySeries extends Activity {
     private void populateListView() {
         listView = (ListView) this.findViewById(R.id.listView);
 
-        listView.setAdapter(new SeriesItemViewAdapter(this, R.layout.list_item,
-                App.environment().seriesProvider().mySeries()));
+        listView.setAdapter(new SeriesItemViewAdapter(this, R.layout.list_item, App.environment()
+                .seriesProvider().mySeries()));
     }
 
     /**
@@ -97,24 +99,56 @@ public class MySeries extends Activity {
     private void setupItemClickListener() {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                    int position, long id) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
                 Intent intent = new Intent(view.getContext(), SeriesView.class);
-                
-                intent.putExtra("series id",
-                        ((Series) parent.getItemAtPosition(position)).getId());
+
+                intent.putExtra("series id", ((Series) parent.getItemAtPosition(position)).getId());
                 intent.putExtra("series name",
                         ((Series) parent.getItemAtPosition(position)).getName());
                 try {
                     startActivity(intent);
                 } catch (Exception e) {
-                    TextView tv =
-                            (TextView) MySeries.this
-                                    .findViewById(R.id.seasonsButton);
+                    TextView tv = (TextView) MySeries.this.findViewById(R.id.seasonsButton);
                     tv.setText(e.getClass() + " " + e.getMessage());
                 }
             }
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my_series_options_menu, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.addSeriesMenuItem:
+                showSearchActivity();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+
+    }
+
+    private void showSearchActivity() {
+
+        Intent intent = new Intent(this, SeriesSearchView.class);
+
+        try {
+            startActivity(intent);
+        } catch (Exception e) {
+            Log.e("MySeries", "Unable to start search activity"); //XXX
+        }
+
+    }
+
 }
