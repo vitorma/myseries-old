bea9b9dabdb793e26500c9a325fab973d716dcc1
Cleber Goncalves de Sousa <cgs.ccc@gmail.com>
2011-10-17 01:54:22 -0200

    Just flowers.

--- aweseries/src/br/edu/ufcg/aweseries/gui/EpisodeView.java
+++ aweseries/src/br/edu/ufcg/aweseries/gui/EpisodeView.java
@@ -18,7 +18,6 @@ public class EpisodeView extends Activity {
     private TextView episodeDirector;
     private TextView episodeWriter;
     private TextView episodeGuestStars;
-
     private boolean loaded = false;
     private ProgressDialog dialog;
 
@@ -26,7 +25,6 @@ public class EpisodeView extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.episode_view);
-
         this.episodeName = (TextView) findViewById(R.id.episodeNameTextView);
         this.episodeName.setText(R.string.unnamedEpisode);
         this.episodeFirstAired = (TextView) findViewById(R.id.episodeFirstAiredTextView);
@@ -34,57 +32,41 @@ public class EpisodeView extends Activity {
         this.episodeDirector = (TextView) findViewById(R.id.episodeDirectorTextView);
         this.episodeWriter = (TextView) findViewById(R.id.episodeWriterTextView);
         this.episodeGuestStars = (TextView) findViewById(R.id.episodeGuestStarsTextView);
-
         this.populateView();
     }
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
-
         if (!loaded && hasFocus) {
             showProgressDialog();
-
             final Handler mHandler = new Handler();
-
             final Runnable mPopulateView = new Runnable() {
                 @Override
                 public void run() {
                     downloadDescription();
                     dismissProgressDialog();
                 }
-
             };
-
             Thread t = new Thread() {
                 @Override
                 public void run() {
                     mHandler.post(mPopulateView);
                 }
             };
-
             t.start();
-
             loaded = true;
         }
     }
 
-    /**
-     * Populates seriesNameTextView and seriesReviewTextField with the data
-     * retrieved from TheTVDB database.
-     */
     private void populateView() {
         Bundle extras = getIntent().getExtras();
-
         if (extras != null) {
             this.episodeId = extras.getString("episode id");
             this.episodeName.setText(extras.getString("episode name"));
         }
     }
 
-    /**
-     * Loads the description from database.
-     */
     private void downloadDescription() {
             Episode episode = seriesProvider().getEpisode(this.episodeId);
             this.episodeName.setText(episode.getName());
@@ -95,26 +77,14 @@ public class EpisodeView extends Activity {
             this.episodeOverview.setText(episode.getOverview());
     }
 
-    /**
-     * Shows progress dialog.
-     */
     private void showProgressDialog() {
-        this.dialog =
-                ProgressDialog
-                        .show(EpisodeView.this, "", "Downloading...", true);
-
+        this.dialog = ProgressDialog.show(EpisodeView.this, "", "Downloading...", true);
     }
 
-    /**
-     * Destroys progress dialog.
-     */
     private void dismissProgressDialog() {
         this.dialog.dismiss();
     }
 
-    /**
-     * @return the app's series provider
-     */
     private SeriesProvider seriesProvider() {
         return App.environment().seriesProvider();
     }
