a1befd4ac3fb217b7a30f7add68ef0831841d2ef
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2011-11-01 15:43:33 -0300

    Upcoming Episodes Activity now uses episode_alone_list_item

--- aweseries/src/br/edu/ufcg/aweseries/gui/UpcomingEpisodesActivity.java
+++ aweseries/src/br/edu/ufcg/aweseries/gui/UpcomingEpisodesActivity.java
@@ -12,11 +12,13 @@ import br.edu.ufcg.aweseries.model.Season;
 import br.edu.ufcg.aweseries.model.Series;
 import android.app.ListActivity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.TextView;
@@ -30,7 +32,7 @@ public class UpcomingEpisodesActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.list_without_toolbar);
         this.populateView();
-        //        this.setupItemClickListener();
+        this.setUpEpisodeItemClickListener();
     }
 
     private void populateView() {
@@ -45,24 +47,22 @@ public class UpcomingEpisodesActivity extends ListActivity {
 
     private void setUpListAdapter() {
         EpisodeItemViewAdapter dataAdapter = new EpisodeItemViewAdapter(this,
-                R.layout.episode_list_item, this.seriesProvider().upcoming());
+                R.layout.episode_alone_list_item, this.seriesProvider().upcoming());
         dataAdapter.sort(comparator);
         this.setListAdapter(dataAdapter);
     }
 
-    /*
-    private void setupItemClickListener() {
+    private void setUpEpisodeItemClickListener() {
         this.getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 final Intent intent = new Intent(view.getContext(), EpisodeView.class);
                 final Episode episode = (Episode) parent.getItemAtPosition(position);
                 intent.putExtra("episode id", episode.getId());
-                EpisodesView.this.startActivity(intent);
+                startActivity(intent);
             }
         });
     }
-    */
 
     private SeriesProvider seriesProvider() {
         return App.environment().seriesProvider();
@@ -96,23 +96,24 @@ public class UpcomingEpisodesActivity extends ListActivity {
             if (itemView == null) {
                 final LayoutInflater li = (LayoutInflater) UpcomingEpisodesActivity.this
                         .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                itemView = li.inflate(R.layout.episode_list_item, null);
+                itemView = li.inflate(R.layout.episode_alone_list_item, null);
             }
 
             // get views for the episodes fields
-            final TextView nameTextView = (TextView) itemView
-                    .findViewById(R.id.episodeNameTextView);
-            final TextView numberTextView = (TextView) itemView
-                    .findViewById(R.id.episodeNumberTextView);
-            final TextView dateTextView = (TextView) itemView
-                    .findViewById(R.id.episodeDateTextView);
-            final CheckBox isViewedCheckBox = (CheckBox) itemView
-                    .findViewById(R.id.episodeIsViewedCheckBox);
+            final TextView nameTextView = (TextView) itemView.findViewById(R.id.episodeNameTextView);
+            final TextView seriesTextView = (TextView) itemView.findViewById(R.id.episodeSeriesTextView);
+            final TextView seasonEpisodeTextView = (TextView) itemView.findViewById(R.id.episodeSeasonEpisodeTextView);
+            final TextView dateTextView = (TextView) itemView.findViewById(R.id.episodeDateTextView);
+            final CheckBox isViewedCheckBox = (CheckBox) itemView.findViewById(R.id.episodeIsViewedCheckBox);
 
             // load episode data
             final Episode episode = this.getItem(position);
+            final Series series = UpcomingEpisodesActivity.this.seriesProvider().getSeries(episode.getSeriesId());
+            final Season season = series.getSeasons().getSeason(episode.getSeasonNumber());
+
             nameTextView.setText(episode.getName());
-            numberTextView.setText(String.format("Episode %02d", episode.getNumber()));
+            seriesTextView.setText(series.getName());
+            seasonEpisodeTextView.setText(String.format("Season %02d - Episode %02d", season.getNumber(), episode.getNumber()));
             dateTextView.setText(episode.getFirstAiredAsString());
             isViewedCheckBox.setChecked(episode.wasSeen());
 
