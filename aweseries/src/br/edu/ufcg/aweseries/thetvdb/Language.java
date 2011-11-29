11ae18827b9f19f8d454c02fbdba36cbc67163eb
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2011-11-29 02:09:15 -0300

    theTVDB.Language enumeration for the languages that TheTVDB supports

---
+++ aweseries/src/br/edu/ufcg/aweseries/thetvdb/Language.java
@@ -0,0 +1,28 @@
+package br.edu.ufcg.aweseries.thetvdb;
+
+public enum Language {
+    DA,
+    FI,
+    NL,
+    DE,
+    IT,
+    ES,
+    FR,
+    PL,
+    HU,
+    EL,
+    TR,
+    RU,
+    HE,
+    JA,
+    PT,
+    ZH,
+    CS,
+    SL,
+    HR,
+    KO,
+    EN,
+    SV,
+    NO
+}
+
