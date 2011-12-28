e738a3295dbfc68439b710d9c28410f423949a3e
Tiago Reul <reultiago@gmail.com>
2011-12-28 02:38:44 -0300

    Replaces old license

--- aweseries-acceptancetest/src/br/edu/ufcg/aweseries/test/integration/TheTVDBStreamFactoryTest.java
+++ aweseries-acceptancetest/src/br/edu/ufcg/aweseries/test/integration/TheTVDBStreamFactoryTest.java
@@ -1,31 +1,24 @@
 /*
  *   TheTVDBStreamFactoryTest.java
  *
- *   Copyright 2011 Cleber Gonçalves de Sousa, Gabriel Assis Bezerra
- *                  and Tiago Almeida Reul
+ *   Copyright 2012 MySeries Team.
  *
- *   All rights reserved.
+ *   This file is part of MySeries.
  *
- *   This file is part of aweseries.
- *
- *   aweseries is free software: you can redistribute it and/or modify
+ *   MySeries is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation, either version 3 of the License, or
  *   (at your option) any later version.
  *
- *   aweseries is distributed in the hope that it will be useful,
+ *   MySeries is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *   GNU General Public License for more details.
  *
  *   You should have received a copy of the GNU General Public License
- *   along with aweseries.  If not, see <http://www.gnu.org/licenses/>.
- *
- *   Contributors:
- *      Cleber Gonçalves de Sousa
- *      Gabriel Assis Bezerra
- *      Tiago Almeida Reul
+ *   along with MySeries.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 
 package br.edu.ufcg.aweseries.test.integration;
 
