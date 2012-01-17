d028d1657aacb2044a9f60e9299fa981d9073608
Cleber Goncalves de Sousa <cgs.ccc@gmail.com>
2012-01-17 00:29:27 -0200

    Adds SeasonListener.

---
+++ aweseries/src/br/edu/ufcg/aweseries/model/SeasonListener.java
@@ -0,0 +1,12 @@
+package br.edu.ufcg.aweseries.model;
+
+public interface SeasonListener {
+
+    public void onMarkAsSeen(Season season);
+
+    public void onMarkAsNotSeen(Season season);
+
+    public void onChangeNextEpisodeToSee(Season season);
+
+    public void onMerge(Season season);
+}
