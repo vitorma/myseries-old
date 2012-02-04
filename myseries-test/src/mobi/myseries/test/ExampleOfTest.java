fe494b372a37b2f49315e66c4bc5b3284b19d0d8
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2012-02-04 18:47:48 -0300

    Tests now passing and adds myseries to myseries-test build path

--- myseries-test/src/mobi/myseries/test/ExampleOfTest.java
+++ myseries-test/src/mobi/myseries/test/ExampleOfTest.java
@@ -1,10 +1,12 @@
 package mobi.myseries.test;
 
+import mobi.myseries.ATestClass;
+import junit.framework.Assert;
 import junit.framework.TestCase;
 
 public class ExampleOfTest extends TestCase {
 
     public void testExample() {
-        fail("must fail");
+        Assert.assertNull(new ATestClass().testString);
     }
 }
