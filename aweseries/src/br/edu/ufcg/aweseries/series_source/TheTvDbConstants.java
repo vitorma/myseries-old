6665a596441fed5ef38b294582f6106e5c1c877b
Cleber Goncalves de Sousa <cgs.ccc@gmail.com>
2012-02-11 14:52:15 -0200

    Creates TheTvDbConstants.

---
+++ aweseries/src/br/edu/ufcg/aweseries/series_source/TheTvDbConstants.java
@@ -0,0 +1,8 @@
+package br.edu.ufcg.aweseries.series_source;
+
+import java.text.SimpleDateFormat;
+
+public class TheTvDbConstants {
+    public static final String API_KEY = "6F2B5A871C96FB05";
+    public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+}
