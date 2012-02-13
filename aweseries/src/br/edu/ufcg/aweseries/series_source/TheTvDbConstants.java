68a746d1f4f6d70a633b9954f4a6c9d5aa493931
Cleber Goncalves de Sousa <cgs.ccc@gmail.com>
2012-02-13 14:13:20 -0200

    Adds TheTvDbConstants#DEFAULT_LANGUAGE.

--- aweseries/src/br/edu/ufcg/aweseries/series_source/TheTvDbConstants.java
+++ aweseries/src/br/edu/ufcg/aweseries/series_source/TheTvDbConstants.java
@@ -26,4 +26,5 @@ import java.text.SimpleDateFormat;
 public class TheTvDbConstants {
     public static final String API_KEY = "6F2B5A871C96FB05";
     public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+    public static final Language DEFAULT_LANGUAGE = Language.ENGLISH;
 }
