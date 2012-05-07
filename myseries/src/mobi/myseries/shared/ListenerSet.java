c739dbb2cc20d5b813cb204d53abe0cd8bce8dd9
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2012-05-07 01:44:50 -0300

    Upgrades mockito to 1.9; ListenerSet is now our implementation of a linked list for weakreferences

--- myseries/src/mobi/myseries/shared/ListenerSet.java
+++ myseries/src/mobi/myseries/shared/ListenerSet.java
@@ -21,26 +21,37 @@
 
 package mobi.myseries.shared;
 
+import java.lang.ref.WeakReference;
 import java.util.Iterator;
-import java.util.WeakHashMap;
+import java.util.NoSuchElementException;
 
 
 public class ListenerSet<L> implements Iterable<L> {
-    // It uses a HashMap because that is the only already implemented collection of weak references
-    private WeakHashMap<L, Object> listeners;
+    private ListenerNode firstSentinel = new ListenerNode(null);
+    private ListenerNode lastSentinel = new ListenerNode(null);
 
     public ListenerSet() {
-        this.listeners = new WeakHashMap<L, Object>();
+        this.firstSentinel = new ListenerNode(null);
+        this.lastSentinel = new ListenerNode(null);
+
+        this.firstSentinel.setNext(this.lastSentinel);
+        this.lastSentinel.setPrevious(this.firstSentinel);
     }
 
     public boolean register(L listener) {
         Validate.isNonNull(listener, "listener");
 
-        if (this.listeners.keySet().contains(listener)) {
+        if (isRegistered(listener)) {
             return false;
         }
 
-        this.listeners.put(listener, null);
+        ListenerNode newLastListener = new ListenerNode(listener);
+        ListenerNode oldLastListener = this.lastSentinel.previous();
+
+        oldLastListener.setNext(newLastListener);
+        newLastListener.setPrevious(oldLastListener);
+        newLastListener.setNext(this.lastSentinel);
+        this.lastSentinel.setPrevious(newLastListener);
 
         return true;
     }
@@ -48,16 +59,109 @@ public class ListenerSet<L> implements Iterable<L> {
     public boolean deregister(L listener) {
         Validate.isNonNull(listener, "listener");
 
-        if (this.listeners.keySet().contains(listener)) {
-            this.listeners.remove(listener);
-            return true;
+        for (Iterator<L> it = this.iterator(); it.hasNext();) {
+            if (it.next() == listener) {
+                it.remove();
+                return true;
+            }
         }
 
         return false;
     }
 
+    private boolean isRegistered(L listener) {
+        for (Iterator<L> it = this.iterator(); it.hasNext();) {
+            if (it.next() == listener) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     @Override
     public Iterator<L> iterator() {
-        return this.listeners.keySet().iterator();
+        return new ListenerIterator(this.firstSentinel);
+    }
+
+    private class ListenerNode {
+        private WeakReference<L> listener;
+
+        private ListenerNode next;
+        private ListenerNode previous;
+
+        public ListenerNode(L listener) {
+            this.listener = new WeakReference<L>(listener);
+        }
+
+        public L listener() {
+            return this.listener.get();
+        }
+
+        public ListenerNode next() {
+            return this.next;
+        }
+
+        public void setNext(ListenerNode next) {
+            this.next = next;
+        }
+
+        public ListenerNode previous() {
+            return this.previous;
+        }
+
+        public void setPrevious(ListenerNode previous) {
+            this.previous = previous;
+        }
+    }
+
+    private class ListenerIterator implements Iterator<L> {
+        private ListenerNode thisListener;
+
+        public ListenerIterator(ListenerNode firstListener) {
+            this.thisListener = firstListener;
+        }
+        
+        private ListenerNode nextListener() {
+            return this.thisListener.next();
+        }
+
+        private ListenerNode previousListener() {
+            return this.thisListener.previous();
+        }
+
+        @Override
+        public boolean hasNext() {
+            this.removeNextCollectedListeners();
+
+            return this.nextListener() != lastSentinel;
+        }
+
+        private void removeNextCollectedListeners() {
+            for (ListenerNode nextListener = this.nextListener();
+                    nextListener != lastSentinel && nextListener.listener() == null;
+                    nextListener = this.nextListener()) {
+
+                new ListenerIterator(nextListener).remove();
+            }
+        }
+
+        @Override
+        public L next() {
+            if (!this.hasNext()) {
+                throw new NoSuchElementException();
+            }
+
+            this.thisListener = this.nextListener();
+            return this.thisListener.listener();
+        }
+
+        @Override
+        public void remove() {
+            ListenerNode nextListener = this.nextListener();
+            ListenerNode previousListener = this.previousListener();
+
+            previousListener.setNext(nextListener);
+            nextListener.setPrevious(previousListener);
+        }
     }
 }
