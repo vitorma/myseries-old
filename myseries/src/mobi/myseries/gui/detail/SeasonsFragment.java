5fcb9e5cf0b1e10ee3e05e62ce0d81afe06bcf23
Cleber Goncalves de Sousa <cgs.ccc@gmail.com>
2012-05-09 22:56:15 -0300

    Removes the EpisodeFragment from the SeriesOverviewActivity.

--- myseries/src/mobi/myseries/gui/detail/SeasonsFragment.java
+++ myseries/src/mobi/myseries/gui/detail/SeasonsFragment.java
@@ -3,12 +3,9 @@ package mobi.myseries.gui.detail;
 import mobi.myseries.R;
 import mobi.myseries.application.App;
 import mobi.myseries.application.SeriesProvider;
-import mobi.myseries.domain.model.Episode;
 import mobi.myseries.domain.model.Series;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,18 +14,12 @@ import android.widget.ExpandableListView;
 import com.actionbarsherlock.app.SherlockFragment;
 
 public class SeasonsFragment extends SherlockFragment {
-    private static final String CURRENT_GROUP = "currentGroup";
-    private static final String CURRENT_CHILD = "currentChild";
     private static final String SERIES_ID = "seriesId";
-    private static final int INVALID_CHILD = -1;
     private static final SeriesProvider SERIES_PROVIDER = App.environment().seriesProvider();
 
     private int seriesId;
     private ExpandableListView list;
     private SeasonsExpandableAdapter adapter;
-    private boolean dualPane;
-    private int currentGroup;
-    private int currentChild;
 
     public static SeasonsFragment newInstance(int seriesId) {
         SeasonsFragment seasonsFragment = new SeasonsFragment();
@@ -44,21 +35,12 @@ public class SeasonsFragment extends SherlockFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (savedInstanceState != null) {
-            this.currentGroup = this.getArguments().getInt(CURRENT_GROUP);
-            this.currentChild = this.getArguments().getInt(CURRENT_CHILD);
-        } else {
-            this.currentChild = INVALID_CHILD;
-        }
-
         this.seriesId = this.getArguments().getInt(SERIES_ID);
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
         outState.putInt(SERIES_ID, this.seriesId);
-        outState.putInt(CURRENT_GROUP, this.currentGroup);
-        outState.putInt(CURRENT_CHILD, this.currentChild);
         super.onSaveInstanceState(outState);
     }
 
@@ -75,29 +57,17 @@ public class SeasonsFragment extends SherlockFragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        FragmentTransaction ft = this.getFragmentManager().beginTransaction();
-
-        Fragment f = this.getFragmentManager().findFragmentById(R.id.overview_details);
-
-        if (f != null) {
-            ft.remove(f);
-        }
-
-        ft.commit();
-
         Series series = SERIES_PROVIDER.getSeries(this.seriesId);
 
         this.adapter = new SeasonsExpandableAdapter(this.getActivity(), series);
+
         this.list = (ExpandableListView) this.getActivity().findViewById(R.id.seasons);
         this.list.setAdapter(this.adapter);
-        this.list.setGroupIndicator(null);
 
         this.list.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
             @Override
             public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
-                SeasonsFragment.this.currentGroup = groupPosition;
-                SeasonsFragment.this.currentChild = childPosition;
-                SeasonsFragment.this.showDetails();
+                SeasonsFragment.this.showDetailsOf(groupPosition, childPosition);
                 return true;
             }
         });
@@ -116,51 +86,13 @@ public class SeasonsFragment extends SherlockFragment {
                 SeasonsFragment.this.list.setSelectedGroup(groupPosition);
             }
         });
-
-        View detailsFrame = this.getActivity().findViewById(R.id.overview_details);
-        this.dualPane = detailsFrame != null && detailsFrame.getVisibility() == View.VISIBLE;
-
-        if (!this.dualPane) {
-            return;
-        }
-
-        if (this.currentChild != INVALID_CHILD) {
-            this.showDetails();
-            return;
-        }
-
-        Episode nextToSee = series.nextEpisodeToSee(true);
-        if (nextToSee != null) {
-            this.currentGroup = this.adapter.groupPosition(nextToSee);
-            this.currentChild = this.adapter.childPosition(nextToSee);
-            this.showDetails();
-            return;
-        }
-
-        Episode lastEpisode = series.lastEpisode();
-        this.currentGroup = this.adapter.groupPosition(lastEpisode);
-        this.currentChild = this.adapter.childPosition(lastEpisode);
-        this.showDetails();
     }
 
-    private void showDetails() {
-        int seasonNumber = this.adapter.seasonNumber(this.currentGroup);
-        int episodeNumber = this.adapter.episodeNumber(this.currentChild);
+    private void showDetailsOf(int currentGroupPosition, int childPosition) {
+        int seasonNumber = this.adapter.seasonNumber(currentGroupPosition);
+        int episodeNumber = this.adapter.episodeNumber(childPosition);
 
-        if (!this.dualPane) {
-            Intent intent = EpisodeDetailsActivity.newIntent(this.getActivity(), this.seriesId, seasonNumber, episodeNumber);
-            this.startActivity(intent);
-            return;
-        }
-
-        this.list.setSelectedChild(this.currentGroup, this.currentChild, true);
-
-        EpisodeFragment details = (EpisodeFragment) this.getFragmentManager().findFragmentById(R.id.overview_details);
-
-        details = EpisodeFragment.newInstance(this.seriesId, seasonNumber, episodeNumber);
-        FragmentTransaction ft = getFragmentManager().beginTransaction();
-        ft.replace(R.id.overview_details, details);
-        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-        ft.commit();
+        Intent intent = EpisodeDetailsActivity.newIntent(this.getActivity(), this.seriesId, seasonNumber, episodeNumber);
+        this.startActivity(intent);
     }
 }
