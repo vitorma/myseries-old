6f898455779508b8a70aa66b627e4fefd8ffeed6
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2012-11-21 17:51:55 -0300

    Implements ImageRepositoryCache

---
+++ myseries-unittest-junit4/src/mobi/myseries/test/unit/domain/repository/ImageRepositoryCacheTest.java
@@ -0,0 +1,213 @@
+package mobi.myseries.test.unit.domain.repository;
+
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+
+import mobi.myseries.domain.repository.ImageRepositoryCache;
+import mobi.myseries.domain.repository.ImageStorage;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import android.graphics.Bitmap;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(Bitmap.class)
+public class ImageRepositoryCacheTest {
+
+    private static long TEST_SLEEPING_INTERVAL_FOR_ASYNC_OPERATIONS_TO_COMPLETE_IN_MILLIS = 10;
+
+    private static int NOT_USED_IMAGE_ID = 0;
+    private static int ID_OF_THE_FIRST_SAVED_IMAGE = 1;
+    private Bitmap DEFAULT_IMAGE = PowerMockito.mock(Bitmap.class);  // This is not static because of a PowerMockito
+                                                                     // issue when the tests are run from ant.
+    private Bitmap DEFAULT_IMAGE2 = PowerMockito.mock(Bitmap.class);
+
+    private ImageStorage cachedRepository;
+    private ImageRepositoryCache cache;
+
+    @Before
+    public void setUp() {
+        this.cachedRepository = mock(ImageStorage.class);
+
+        this.cache = new ImageRepositoryCache(this.cachedRepository);
+    }
+
+    /* Construction */
+
+    @Test(expected=IllegalArgumentException.class)
+    public void constructingItWithANullRepositoryCausesIllegalArgumentException() {
+        new ImageRepositoryCache(null);
+    }
+
+    /* Pre-fetching during construction */
+
+    @Test
+    public void theAlreadySavedImagesMustBePrefetchedDuringConstruction() {
+        List<Integer> returnedImages = Arrays.asList(1, 2, 3, 4, 5);
+
+        ImageStorage cachedRepository = mock(ImageStorage.class);
+        when(cachedRepository.savedImages()).thenReturn(returnedImages);
+
+        new ImageRepositoryCache(cachedRepository);
+
+        verify(cachedRepository).savedImages();
+        for (int image : returnedImages) {
+            verify(cachedRepository).fetch(image);
+        }
+    }
+
+    /* Fetching */
+
+    @Test
+    public void fetchingANotSavedImageReturnsNull() {
+        assertThat(this.cache.fetch(NOT_USED_IMAGE_ID), nullValue());
+    }
+
+    @Test
+    public void fetchingAnAlreadySavedImageImmediatelyAfterConstructionDoesNotTouchTheCachedRepository() {
+        List<Integer> returnedImages = Arrays.asList(1, 2, 3, 4, 5);
+        int fetchedImage = 1;
+
+        ImageStorage cachedRepository = mock(ImageStorage.class);
+        when(cachedRepository.savedImages()).thenReturn(returnedImages);
+
+        new ImageRepositoryCache(cachedRepository).fetch(fetchedImage);
+
+        verify(cachedRepository, times(1)).fetch(fetchedImage);  // only for prefetching
+    }
+
+    @Test
+    public void fetchingAnAlreadySavedImageImmediatelyAfterConstructionReturnsTheSavedImage() {
+        List<Integer> returnedImages = Arrays.asList(1, 2, 3, 4, 5);
+        int fetchedImageId = 1;
+
+        ImageStorage cachedRepository = mock(ImageStorage.class);
+        when(cachedRepository.savedImages()).thenReturn(returnedImages);
+        when(cachedRepository.fetch(fetchedImageId)).thenReturn(DEFAULT_IMAGE);
+
+        Bitmap fetchedImage = new ImageRepositoryCache(cachedRepository).fetch(fetchedImageId);
+
+        assertThat(fetchedImage, sameInstance(DEFAULT_IMAGE));
+    }
+
+    @Test
+    public void fetchingAJustSavedImageReturnsTheSavedImage() {
+        int imageId = ID_OF_THE_FIRST_SAVED_IMAGE;
+        Bitmap savedImage = DEFAULT_IMAGE;
+
+        this.cache.save(imageId, savedImage);
+
+        Bitmap fetchedImage = this.cache.fetch(imageId);
+
+        assertThat(fetchedImage, sameInstance(savedImage));
+    }
+
+    @Test
+    public void fetchingAJustReplacedImageReturnsTheLastSavedImage() {
+        int imageId = ID_OF_THE_FIRST_SAVED_IMAGE;
+        Bitmap firstSavedImage = DEFAULT_IMAGE;
+        Bitmap secondSavedImage = DEFAULT_IMAGE;
+
+        this.cache.save(imageId, firstSavedImage);
+        this.cache.save(imageId, secondSavedImage);
+
+        Bitmap fetchedImage = this.cache.fetch(imageId);
+
+        assertThat(fetchedImage, sameInstance(secondSavedImage));
+    }
+
+    @Test
+    public void fetchingAJustDeletedImageReturnsNull() {
+        int imageId = ID_OF_THE_FIRST_SAVED_IMAGE;
+        Bitmap savedImage = DEFAULT_IMAGE;
+
+        this.cache.save(imageId, savedImage);
+        this.cache.delete(imageId);
+
+        assertThat(this.cache.fetch(imageId), nullValue());
+    }
+
+    /* Saving */
+
+    @Test(expected=IllegalArgumentException.class)
+    public void savingANullImageShouldThrowAnIllegalArgumentException() {
+        this.cache.save(ID_OF_THE_FIRST_SAVED_IMAGE, null);
+    }
+
+    @Test
+    public void savingAnImageForTheFirstTimeTouchesTheCachedRepository() {
+        int imageId = ID_OF_THE_FIRST_SAVED_IMAGE;
+        Bitmap savedImage = DEFAULT_IMAGE;
+
+        this.cache.save(imageId, savedImage);
+
+        verify(this.cachedRepository, never()).save(imageId, savedImage);
+        waitForAsynchronousOperations();
+        verify(this.cachedRepository).save(imageId, savedImage);
+    }
+
+    @Test
+    public void savingAnImageForTheSecondTimeTouchesTheCachedRepositoryTwice() {
+        int imageId = ID_OF_THE_FIRST_SAVED_IMAGE;
+        Bitmap firstlySavedImage = DEFAULT_IMAGE;
+        Bitmap secondlySavedImage = DEFAULT_IMAGE2;
+
+        this.cache.save(imageId, firstlySavedImage);
+        this.cache.save(imageId, secondlySavedImage);
+
+        verify(this.cachedRepository, never()).save(imageId, firstlySavedImage);
+        verify(this.cachedRepository, never()).save(imageId, secondlySavedImage);
+        waitForAsynchronousOperations();
+        verify(this.cachedRepository).save(imageId, firstlySavedImage);
+        verify(this.cachedRepository).save(imageId, secondlySavedImage);
+    }
+
+    /* Deleting */
+
+    @Test
+    public void deletingAnImageMustDeleteItFromTheCachedRepository() {
+        int deletedImageId = ID_OF_THE_FIRST_SAVED_IMAGE;
+
+        this.cache.delete(deletedImageId);
+
+        verify(this.cachedRepository, never()).delete(deletedImageId);
+        waitForAsynchronousOperations();
+        verify(this.cachedRepository).delete(deletedImageId);
+    }
+
+    /* Saved Images */
+
+    @Test
+    public void queryingTheCollectionOfSavedImagesMustNotTouchTheCachedRepository() {
+        reset(this.cachedRepository);  // to ignore prefetching
+
+        this.cache.savedImages();
+
+        verify(this.cachedRepository, never()).savedImages();
+    }
+
+    /* Test tools */
+
+    private static void waitForAsynchronousOperations() {
+        try {
+            Thread.sleep(TEST_SLEEPING_INTERVAL_FOR_ASYNC_OPERATIONS_TO_COMPLETE_IN_MILLIS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ myseries/src/mobi/myseries/domain/repository/ImageRepositoryCache.java
@@ -0,0 +1,86 @@
+package mobi.myseries.domain.repository;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import mobi.myseries.shared.Validate;
+import android.graphics.Bitmap;
+import android.util.SparseArray;
+
+public class ImageRepositoryCache implements ImageStorage {
+
+    private SparseArray<Bitmap> cachedImages;
+    private Set<Integer> cachedImagesIds;  // this is here because there so far (2012-11-21) there is no easy way to
+                                           // get the list of keys in a SparseArray.
+
+    private ExecutorService threadExecutor;
+    private ImageStorage cachedRepository;
+
+    public ImageRepositoryCache(ImageStorage cachedRepository) {
+        Validate.isNonNull(cachedRepository, "cachedRepository");
+
+        this.cachedRepository = cachedRepository;
+        this.threadExecutor = Executors.newSingleThreadExecutor();
+        this.cachedImages = new SparseArray<Bitmap>();
+        this.cachedImagesIds = new HashSet<Integer>();
+
+        this.loadImagesFromCachedRepository();
+    }
+
+    private void loadImagesFromCachedRepository() {
+        for (int image : this.cachedRepository.savedImages()) {
+            this.cachedImages.put(image, this.cachedRepository.fetch(image));
+        }
+    }
+
+    @Override
+    public void save(int id, Bitmap image) {
+        Validate.isNonNull(image, "image");
+
+        this.cachedImages.put(id, image);
+        this.cachedImagesIds.add(id);
+        this.threadExecutor.execute(this.saveImageInTheCachedRepository(id, image));
+    }
+
+    private Runnable saveImageInTheCachedRepository(final int id, final Bitmap image) {
+        return new Runnable() {
+
+            @Override
+            public void run() {
+                ImageRepositoryCache.this.cachedRepository.save(id, image);
+            }
+        };
+    }
+
+    @Override
+    public Bitmap fetch(int id) {
+        return this.cachedImages.get(id);
+    }
+
+    @Override
+    public void delete(int id) {
+        this.cachedImages.delete(id);
+        this.cachedImagesIds.remove(id);
+        this.threadExecutor.execute(this.deleteImageFromTheCachedRepository(id));
+    }
+
+    private Runnable deleteImageFromTheCachedRepository(final int id) {
+        return new Runnable() {
+
+            @Override
+            public void run() {
+                ImageRepositoryCache.this.cachedRepository.delete(id);
+            }
+        };
+    }
+
+    @Override
+    public Collection<Integer> savedImages() {
+        return Collections.unmodifiableCollection(this.cachedImagesIds);
+        //return this.cachedRepository.savedImages();
+    }
+}
