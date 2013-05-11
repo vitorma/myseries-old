be36f4b97b65374b9f27617886ae31723d073f11
Tiago A. Reul <tiagoreul@gmail.com>
2013-05-11 12:11:40 -0300

    Make poster resize (bicubic interpolation) a little faster

--- myseries/src/mobi/myseries/shared/imageprocessing/BitmapResizer.java
+++ myseries/src/mobi/myseries/shared/imageprocessing/BitmapResizer.java
@@ -2,8 +2,10 @@ package mobi.myseries.shared.imageprocessing;
 
 import mobi.myseries.shared.Validate;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.util.Log;
 
-public class BitmapResizer {
+public final class BitmapResizer {
     final Bitmap bitmap;
 
     public BitmapResizer(Bitmap bitmap) {
@@ -18,60 +20,64 @@ public class BitmapResizer {
         //TODO (Cleber) This method is currently very expensive.
         //              Due to it, add a series can take more than one minute.
         //              Improve it and uncomment next lines.
+        
+        long start = System.currentTimeMillis();
 
-//        Bitmap destiny = Bitmap.createBitmap(targetWidth, targetHeight, Bitmap.Config.ARGB_8888);
-//
-//        float scaleFactor =
-//                Math.min((float) this.bitmap.getWidth() / targetWidth,
-//                        (float) this.bitmap.getHeight() / targetHeight);
-//
-//        for (int y = 0; y < targetHeight; ++y) {
-//            for (int x = 0; x < targetWidth; ++x) {
-//
-//                int sourceX = clamp(0, (int) (scaleFactor * x), this.bitmap.getWidth() - 1);
-//                int sourceY = clamp(0, (int) (scaleFactor * y), this.bitmap.getHeight() - 1);
-//
-//                double dx = x - (sourceX * (1 / scaleFactor));
-//                double dy = y - (sourceY * (1 / scaleFactor));
-//
-//                double red = 0;
-//                double green = 0;
-//                double blue = 0;
-//                for (int m = -2; m < 2; ++m) {
-//                    for (int n = -2; n < 2; ++n) {
-//                        int currentX = clamp(0, sourceX + n, bitmap.getWidth());
-//                        int currentY = clamp(0, sourceY + m, bitmap.getHeight());
-//
-//                        red +=
-//                                ((Color.red(bitmap.getPixel(currentX, currentY))) * r(m - dx) * r(dy
-//                                        - n));
-//                        green +=
-//                                ((Color.green(bitmap.getPixel(currentX, currentY))) * r(m - dx) * r(dy
-//                                        - n));
-//                        blue +=
-//                                ((Color.blue(bitmap.getPixel(currentX, currentY))) * r(m - dx) * r(dy
-//                                        - n));
-//                    }
-//                }
-//
-//                destiny.setPixel(x, y, Color.argb(0xff, (int) red, (int) green, (int) blue));
-//            }
-//        }
+        final Bitmap destiny = Bitmap.createBitmap(targetWidth, targetHeight, Bitmap.Config.ARGB_8888);
 
-        Bitmap destiny = Bitmap.createScaledBitmap(this.bitmap, targetWidth, targetHeight, false);
+        final float scaleFactor = Math.min((float) this.bitmap.getWidth() / targetWidth,
+                        (float) this.bitmap.getHeight() / targetHeight);
+        
+        float dx, dy, red, green, blue;
+        int x, y, m, n;
+        int sourceX, sourceY, currentX, currentY;
+        
+
+        for (y = 0; y < targetHeight; ++y) {
+            for (x = 0; x < targetWidth; ++x) {
+
+                sourceX = clamp(0, (int) (scaleFactor * x), this.bitmap.getWidth() - 1);
+                sourceY = clamp(0, (int) (scaleFactor * y), this.bitmap.getHeight() - 1);
+
+                dx = x - (sourceX * (1 / scaleFactor));
+                dy = y - (sourceY * (1 / scaleFactor));
+
+                red = 0;
+                green = 0;
+                blue = 0;
+                for (m = -2; m < 2; ++m) {
+                    for (n = -2; n < 2; ++n) {
+                        currentX = clamp(0, sourceX + n, bitmap.getWidth());
+                        currentY = clamp(0, sourceY + m, bitmap.getHeight());
+
+                        red += ((Color.red(bitmap.getPixel(currentX, currentY))) * r(m - dx) * r(dy - n));
+                        green += ((Color.green(bitmap.getPixel(currentX, currentY))) * r(m - dx) * r(dy - n));
+                        blue += ((Color.blue(bitmap.getPixel(currentX, currentY))) * r(m - dx) * r(dy - n));
+                    }
+                }
+
+                destiny.setPixel(x, y, Color.argb(0xff, (int) red, (int) green, (int) blue));
+            }
+        }
+        
+        long end = System.currentTimeMillis();
+        
+        Log.i(getClass().getName(), "Resize time: " + (end - start) / 1000.0);
+
+        //Bitmap destiny = Bitmap.createScaledBitmap(this.bitmap, targetWidth, targetHeight, false);
         return destiny;
     }
 
-    public int clamp(int lo, int value, int hi) {
+    private int clamp(int lo, int value, int hi) {
         return (value < lo) ? (lo) : ((value > hi) ? (hi) : (value));
     }
 
-    public double r(double x) {
-        return ((1.0 / 6.0) * (((Math.pow(this.p(x + 2.), 3.) - (4. * Math.pow(this.p(x + 1.), 3.))) + (6. * Math
-                .pow(this.p(x), 3.))) - (4. * Math.pow(this.p(x - 1.), 3.))));
+    private float r(float x) {
+        return ((1.0f / 6.0f) * 
+                (((1 * ppow3((x + 2)) - (4 * ppow3((x + 1)))) + (6 * ppow3((x)))) - (4 * ppow3((x - 1)))));
     }
-
-    public double p(double x) {
-        return (x < 0.) ? (0.) : (x);
+    
+    private float ppow3(float x) {
+        return x < 0.01 ? 0: x * x * x;
     }
 }
