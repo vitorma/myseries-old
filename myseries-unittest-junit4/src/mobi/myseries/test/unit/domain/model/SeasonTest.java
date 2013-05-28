e8d268e04590f91d40cd0900d4107c49248bb2b6
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2013-05-27 21:35:45 -0300

    Merge of Season now removes the removed episodes; Season no longer requires seriesId to be >= 0.

--- myseries-unittest-junit4/src/mobi/myseries/test/unit/domain/model/SeasonTest.java
+++ myseries-unittest-junit4/src/mobi/myseries/test/unit/domain/model/SeasonTest.java
@@ -76,8 +76,8 @@ public class SeasonTest {
 
     /* Construction */
 
-    @Test(expected=IllegalArgumentException.class)
-    public void constructingASeasonWithNegativeSeriesIdCausesIllegalArgumentException() {
+    @Test
+    public void constructingASeasonWithNegativeSeriesIdDoesNotCauseException() {
         new Season(-1, 0);
     }
 
@@ -129,7 +129,7 @@ public class SeasonTest {
         Season season = new Season(1, 1).including(mockEpisode(1, 1, 1, 1));
 
         Assert.assertTrue(season.includes(mockEpisode(1, 1, 1, 1)));
-        Assert.assertTrue(season.includes(mockEpisode(2, 1, 1, 1)));
+        Assert.assertTrue(season.includes(mockEpisode(2, 1, 1, 1)));  // even if their ids are different
         Assert.assertFalse(season.includes(mockEpisode(1, 1, 2, 1)));
     }
 
