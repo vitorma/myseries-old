8403dfcd28cb2ddf753c5638e3a2c62f00adc61f
Gabriel Assis Bezerra <gabriel.bezerra@gmail.com>
2013-06-28 23:09:49 -0300

    Telling discrete and continuous notifications

--- myseries/src/mobi/myseries/application/notification/Notification.java
+++ myseries/src/mobi/myseries/application/notification/Notification.java
@@ -3,15 +3,21 @@ package mobi.myseries.application.notification;
 public abstract class Notification {
 
     private final int id;
+    private final boolean isContinuous;
     private final CharSequence message;
 
-    protected Notification(int id, CharSequence message) {
+    protected Notification(int id, boolean isContinuous, CharSequence message) {
         this.id = id;
+        this.isContinuous = isContinuous;
         this.message = message;
     }
 
     public int id() {
         return this.id;
+    }
+
+    public boolean isContinuous() {
+        return this.isContinuous;
     }
 
     public CharSequence message() {
