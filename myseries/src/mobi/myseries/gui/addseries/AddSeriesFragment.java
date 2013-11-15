841131ba12913947679551c5ffbdb0fb7ecf1511
Cleber Goncalves de Sousa <cgs.ccc@gmail.com>
2013-11-15 14:36:14 -0300

    AddSeries: keyboard shows 'Search' instead of 'Done'.

--- myseries/src/mobi/myseries/gui/addseries/AddSeriesFragment.java
+++ myseries/src/mobi/myseries/gui/addseries/AddSeriesFragment.java
@@ -161,7 +161,7 @@ public abstract class AddSeriesFragment extends Fragment {
         mSearchField.setOnEditorActionListener(new OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
                     AddSeriesFragment.this.runService();
                     return true;
                 }
